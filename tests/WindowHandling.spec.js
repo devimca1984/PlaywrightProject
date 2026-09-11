71039a4d9351e3ce153cd0d719cf70f94ec0848f
Devi RP <devistudydevi@gmail.com>
2026-09-12 02:08:04 +0530

    Fix window handling in WindowHandling.spec.js

--- tests/WindowHandling.spec.js
+++ tests/WindowHandling.spec.js
@@ -5,14 +5,15 @@ const page= await context.newPage()
 //Browser Launch
 await page.goto("https://testautomationpractice.blogspot.com/")
 //Window Handling
-const[page1,page2]= await Promise.all([
+const[page2]= await Promise.all([
 context.waitForEvent('page')
-page1.getByRole('button',{name : 'New Tab'}).click()
-])
+await page.getByRole('button',{name : 'New Tab'}).click()
+]);
 
-console.log(await Page1.locator("ul li a").first().textContent())
+console.log(await page2.locator("ul li a").first().textContent())
 await page.getByRole('checkbox',{name : 'Monday'}).check()
 await page.waitForTimeout(5000)
 
 
 })
+
